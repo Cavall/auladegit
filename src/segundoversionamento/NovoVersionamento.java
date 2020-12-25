e177019dbef99376061ae47c558376f31a1fe50c
Cavall <antoniodonizethe@hotmail.com>
2020-12-24 22:04:23 -0300

    Projeto correto substituindo master por main...deu certo

---
+++ src/segundoversionamento/NovoVersionamento.java
@@ -0,0 +1,10 @@
+package segundoversionamento;
+
+public class NovoVersionamento {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Aqui de novo...aprendendo versionamento");
+	}
+
+}
